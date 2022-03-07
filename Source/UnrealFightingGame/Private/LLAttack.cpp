aa45bb72c128afff4197ee47a502dab2e63db409
leolaz99 <leolaz99@gmail.com>
2022-03-07 18:32:03 +0100

    deleted debug lines

--- Source/UnrealFightingGame/Private/LLAttack.cpp
+++ Source/UnrealFightingGame/Private/LLAttack.cpp
@@ -29,7 +29,7 @@ void ULLAttack::Attack()
 	FCollisionQueryParams params;
 
 	bool isHit = UKismetSystemLibrary::SphereTraceSingle(GetWorld(), SocketLocation1, SocketLocation2, 5.f, UEngineTypes::ConvertToTraceType(ECC_Camera), false, actorsToIgnore,
-														 EDrawDebugTrace::ForDuration, hitResult, true, FLinearColor::Red, FLinearColor::Green, 1.f);
+														 EDrawDebugTrace::ForDuration, hitResult, true, FLinearColor::Red, FLinearColor::Green, 0.f);
 
 	if (isHit)
 	{
