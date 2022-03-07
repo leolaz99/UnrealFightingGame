342aaabf6dddb3d845714605e85d3ee112d0ae3d
leolaz99 <leolaz99@gmail.com>
2022-03-07 16:29:36 +0100

    added camera shake

---
+++ Source/UnrealFightingGame/Private/LLCameraShake.cpp
@@ -0,0 +1,2 @@
+#include "LLCameraShake.h"
+
---
+++ Source/UnrealFightingGame/Public/LLCameraShake.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraShake.h"
+#include "LLCameraShake.generated.h"
+
+UCLASS()
+class UNREALFIGHTINGGAME_API ULLCameraShake : public UMatineeCameraShake
+{
+	GENERATED_BODY()
+	
+
+};
